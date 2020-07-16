5927300b7fde87423aabef9654abf39b52e498e2
JeffBcyc <41636467+JeffBcyc@users.noreply.github.com>
2020-07-16 16:16:10 -0400

    EnemyAI
    added some basic enemy ai with nav mesh agent

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    [SerializeField] Transform target;
+    NavMeshAgent navMeshAgent;
+    [SerializeField] float chaseRange = 5f;
+
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
+
+    private void Start()
+    {
+        
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        target = GameObject.Find("Player").transform;
+    }
+
+    private void Update()
+    {
+
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        if (isProvoked)
+        {
+            EngageTarget();
+        } else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+            //navMeshAgent.SetDestination(target.position);
+            //print("Found player, start chasing");
+            //navMeshAgent.Move(target.position);
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        } else if (distanceToTarget > navMeshAgent.stoppingDistance)
+        {
+            AttachTarget();
+        }
+    }
+
+    private void AttachTarget()
+    {
+        print("Attacking Player");
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+
+}
